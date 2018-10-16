34c3959903c9c9a73f5f82781084fda80a72e69f
Philip Sahli <philip@sahli.net>
2018-10-16 21:43:32 +0200

    update create_chart

--- create_chart.sh
+++ create_chart.sh
@@ -4,10 +4,12 @@
 
 # git rm -f *tgz
 
+cd helm
+
 tag=$1
 
 find plone-stack -exec grep -l "version:" {} \; 2>/dev/null| xargs sed -i "" -e "s/\(.*version:\).*/\1 ${tag}/g"
-find plone-stack -exec grep -l "tag:" {} \; 2>/dev/null| xargs sed -i "" -e "s/\(.*tag:\).*/\1 ${tag}/g"
+find plone-stack -exec grep -l "tag: [0-9]" {} \; 2>/dev/null| xargs sed  -i "" -e "s/\(.*tag:\) [0-9].*/\1 ${tag}/g"
 
 helm package plone-stack --version $tag
 
